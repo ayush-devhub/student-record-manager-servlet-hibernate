3996f3efd41e3e2977e6033232820dcdbfedb4ed
ayush-devhub <ayush.devhub@gmail.com>
2025-10-16 23:16:13 +0530

    feat(utility): add DBUtil to provide JDBC connection
    Provides a reusable utility method to establish MySQL connections using DriverManager.

--- src/main/java/utility/DBUtil.java
+++ src/main/java/utility/DBUtil.java
@@ -1,4 +1,16 @@
 package utility;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBUtil {
+    private static String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static String USER = "root";
+    private static String PASSWORD = "mamta93@";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+        
+    }
 }
