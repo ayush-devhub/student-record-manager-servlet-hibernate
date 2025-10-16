78a8efa1582f8db8eabbf33dc6132b8102bd835f
ayush-devhub <ayush.devhub@gmail.com>
2025-10-17 01:05:14 +0530

    feat(controller): add delete servlet to delete entries from DB

--- src/main/java/servlet/DeleteStudentServlet.java
+++ src/main/java/servlet/DeleteStudentServlet.java
@@ -1,4 +1,30 @@
 package servlet;
 
-public class DeleteStudentServlet {
+import dao.DAOImpl;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/delete-student-servlet")
+public class DeleteStudentServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        try{
+            DAOImpl dao = new DAOImpl();
+            if(dao.deleteStudent(id)){
+                HttpSession session = request.getSession();
+                session.setAttribute("message", "Student deleted successfully");
+                response.sendRedirect("view-student-servlet");
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
