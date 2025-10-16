e2ed6b3e44a0f4c4ca91ed6edd32e091d9dee2c4
ayush-devhub <ayush.devhub@gmail.com>
2025-10-16 23:35:14 +0530

    feat(dao): define DAO interface for student CRUD operations
    Provides method contracts for adding, updating, deleting, and fetching students.
    This will be implemented by concrete DAO classes interacting with the database.

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -1,4 +1,13 @@
 package dao;
 
+import model.Student;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface DAO {
+    public boolean addStudent(Student student) throws SQLException;
+    public boolean updateStudent(Student student) throws SQLException;
+    public boolean deleteStudent(int id) throws SQLException;
+    public List<Student> getAllStudents() throws SQLException;
 }
