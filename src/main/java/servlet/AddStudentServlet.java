750fccc89e46f58e6a9a1e417cd81afc41c8d95d
ayush-devhub <ayush.devhub@gmail.com>
2025-10-17 00:45:13 +0530

    feat(model): implement Add Student Servlet class
    It fetches and parses values from add-student.jsp and add's new entry to DB

--- src/main/java/servlet/AddStudentServlet.java
+++ src/main/java/servlet/AddStudentServlet.java
@@ -1,4 +1,35 @@
 package servlet;
 
-public class AddStudentServlet {
+import dao.DAOImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Student;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/add-student-servlet")
+public class AddStudentServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String course = request.getParameter("course");
+
+        try {
+            Student student = new Student(name, email, course);
+            DAOImpl dao = new DAOImpl();
+            boolean status = dao.addStudent(student);
+            if (status) {
+                HttpSession session = request.getSession();
+                session.setAttribute("message", "Student has been successfully added!");
+                response.sendRedirect("view-student-servlet");
+            }
+        } catch (SQLException | ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
